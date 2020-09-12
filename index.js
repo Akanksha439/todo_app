5ce0ffab18fa8a6793a15983e61da41c3ca9e017
akanksha <akanksha439@gmail.com>
2020-09-12 15:59:07 +0530

    install and set up ejs

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app=express();
 const port=8000;
 
 //use espress router
-app.use('/',require('/.routes'));
+app.use('/',require('./routes'));
+
+//setting up ejs
+app.set('view engine','ejs');
+app.set('views','.\views');
 
 app.listen(port,function(err){
     if(err)
